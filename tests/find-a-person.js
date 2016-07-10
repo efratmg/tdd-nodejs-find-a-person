5c76673458934a4302f685554d95d55a9db0f190
efratmg <efratpin@gmail.com>
2016-07-10 14:01:42 +0300

    RED:test 2 failed

--- tests/find-a-person.js
+++ tests/find-a-person.js
@@ -36,8 +36,8 @@ describe('Find a person (location) 1', function() {
 
 describe('Find a person (location) 2', function() {
   it('Given a person name, return if posts (of a map) containing location (in any of a post fields)', function() {
-    var map = new Map (["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley", "efrat "]);
-    var ans = map.find_a_person_location("efrat");
+    var map = new Map (["I met Or A. at Chabad house Bangkok", "We found Or A. R.I.P at Langtang valley", "david "]);
+    var ans = map.find_a_person_location(" ");
     expect(ans).to.be.eql(false);
   });
 });
